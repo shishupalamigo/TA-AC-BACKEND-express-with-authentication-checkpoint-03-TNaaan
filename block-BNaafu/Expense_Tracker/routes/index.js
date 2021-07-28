6d042a4ddb08232207f4d8a45ecad6e02c7886a7
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-07-28 12:59:58 +0530

    Add nodemailer for verification mnails

--- block-BNaafu/Expense_Tracker/routes/index.js
+++ block-BNaafu/Expense_Tracker/routes/index.js
@@ -3,7 +3,9 @@ var passport = require('passport');
 let User = require('../models/User');
 let Income = require('../models/Income');
 let Expense = require('../models/Expense');
-
+var Token = require('../models/Token');
+var nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 var router = express.Router();
 
@@ -37,6 +39,47 @@ router.get('/auth/google/callback', passport.authenticate('google',
       delete req.session.returnTo;
       // res.redirect('/users/dashboard');
     });
- 
+
+// Email confirmation
+
+router.get('/confirmation/:email/:token', function (req, res, next) {
+  Token.findOne({ token: req.params.token }, function (err, token) {
+      // token is not found into database i.e. token may have expired 
+      if (!token){
+          return res.status(400).send({msg:'Your verification link may have expired. Please click on resend for verify your Email.'});
+      }
+      // if token is found then check valid user 
+      else{
+          User.findOne({ _id: token._userId, email: req.params.email }, function (err, user) {
+              // not valid user
+              if (!user){
+                  return res.status(401).send({msg:'We were unable to find a user for this verification. Please SignUp!'});
+              } 
+              // user is already verified
+              else if (user.isVerified){
+                  return res.status(200).send('User has been already verified. Please Login');
+              }
+              // verify user
+              else{
+                  // change isVerified to true
+                  user.isVerified = true;
+                  user.save(function (err) {
+                      // error occur
+                      if(err){
+                          return res.status(500).send({msg: err.message});
+                      }
+                      // account successfully verified
+                      else{
+                        return res.status(200).send('Your account has been successfully verified');
+                      }
+                  });
+              }
+          });
+      }
+      
+  });
+});    
+
+
 
 module.exports = router;
--- block-BNaafu/Expense_Tracker/routes/users.js
+++ block-BNaafu/Expense_Tracker/routes/users.js
@@ -68,65 +68,68 @@ router.post('/register', upload.single('profilePic') ,(req, res, next) => {
         var transporter = nodemailer.createTransport({ 
           service: 'Sendgrid', 
           auth: { 
-                  api_key:process.env.SENDGRID_APIKEY,
-                } 
-            });
+            user: 'apikey', 
+            pass: process.env.SENDGRID_APIKEY,
+          } 
+        });
         var mailOptions = { 
-                  from: 'no-reply@example.com', 
+                  from: 'shisgupal.bhu@gmail.com', 
                   to: user.email, 
                   subject: 'Account Verification Link', 
                   text: 'Hello '+ req.body.name +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.email + '\/' + token.token + '\n\nThank You!\n' 
         };
         transporter.sendMail(mailOptions, function (err) {
-        if (err) { 
-          return res.status(500).send({msg:'Technical Issue!, Please click on resend for verify your Email.'});
+          console.log(mailOptions);
+          if (err) { 
+          return res.status(500).send({msg:'Technical Issue!, Please click on resend to verify your Email.'});
+        } else {
+          req.flash('success', 'A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend token.');
+          res.redirect('/users/login');
         }
-        req.flash('success', 'A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend token.');
-        res.redirect('/users/login');
       });
     });
   });
 })
 
-// Email confirmation
+// // Email confirmation
 
-router.get('/confirmation/:email/:token', function (req, res, next) {
-  Token.findOne({ token: req.params.token }, function (err, token) {
-      // token is not found into database i.e. token may have expired 
-      if (!token){
-          return res.status(400).send({msg:'Your verification link may have expired. Please click on resend for verify your Email.'});
-      }
-      // if token is found then check valid user 
-      else{
-          User.findOne({ _id: token._userId, email: req.params.email }, function (err, user) {
-              // not valid user
-              if (!user){
-                  return res.status(401).send({msg:'We were unable to find a user for this verification. Please SignUp!'});
-              } 
-              // user is already verified
-              else if (user.isVerified){
-                  return res.status(200).send('User has been already verified. Please Login');
-              }
-              // verify user
-              else{
-                  // change isVerified to true
-                  user.isVerified = true;
-                  user.save(function (err) {
-                      // error occur
-                      if(err){
-                          return res.status(500).send({msg: err.message});
-                      }
-                      // account successfully verified
-                      else{
-                        return res.status(200).send('Your account has been successfully verified');
-                      }
-                  });
-              }
-          });
-      }
+// router.get('/confirmation/:email/:token', function (req, res, next) {
+//   Token.findOne({ token: req.params.token }, function (err, token) {
+//       // token is not found into database i.e. token may have expired 
+//       if (!token){
+//           return res.status(400).send({msg:'Your verification link may have expired. Please click on resend for verify your Email.'});
+//       }
+//       // if token is found then check valid user 
+//       else{
+//           User.findOne({ _id: token._userId, email: req.params.email }, function (err, user) {
+//               // not valid user
+//               if (!user){
+//                   return res.status(401).send({msg:'We were unable to find a user for this verification. Please SignUp!'});
+//               } 
+//               // user is already verified
+//               else if (user.isVerified){
+//                   return res.status(200).send('User has been already verified. Please Login');
+//               }
+//               // verify user
+//               else{
+//                   // change isVerified to true
+//                   user.isVerified = true;
+//                   user.save(function (err) {
+//                       // error occur
+//                       if(err){
+//                           return res.status(500).send({msg: err.message});
+//                       }
+//                       // account successfully verified
+//                       else{
+//                         return res.status(200).send('Your account has been successfully verified');
+//                       }
+//                   });
+//               }
+//           });
+//       }
       
-  });
-});
+//   });
+// });
 
 
 // Login
