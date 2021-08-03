1617335c0f423bbf85d30da326bf7ca7216a1d71
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-08-03 11:46:51 +0530

    refactor code

--- block-BNaafu/Expense_Tracker/randomText.js
+++
@@ -1,9 +0,0 @@
-function randomNumber() {
-    let str = "0123456789", str2 = "";
-   for(let i = 0; i <= 5; i++) {
-    str2 += str[Math.floor(Math.random() * 9)];
- }
-return str2;
-}
-
-module.exports = randomNumber;
